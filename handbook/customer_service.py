efc440f84bf7a503a9bbf8f5a5b7669a958562cc
alena <alena@lagoshny.ru>
2021-03-29 10:27:47 +0300

    changed database connection in DataBaseStorage class added method description

--- handbook/customer_service.py
+++ handbook/customer_service.py
@@ -44,6 +44,14 @@ class Customer:
         self.phone = phone
 
 
+class CustomerException(Exception):
+    def __init__(self, message: str) -> None:
+        self.message = message
+
+    def __str__(self) -> str:
+        return self.message
+
+
 class StorageStrategy(ABC):
     @abstractmethod
     def insert_customer(self, customer: Customer) -> None:
@@ -78,9 +86,21 @@ class InMemoryStorage(StorageStrategy):
         return '\n'.join(self.customers)
 
     def insert_customer(self, customer: Customer) -> None:
+        """
+        Inserts a customer instance into the storage
+        :param customer: Customer
+        :return: None
+        """
         self.customers.append(customer)
 
     def find_customer(self, argument_name: str, argument_value: str) -> Customer:
+        """
+        Searches for a customer in the storage by argument name and value
+        and returns the result
+        :param argument_name: the name of the argument to search for
+        :param argument_value: the value of the argument to search for
+        :return: Customer
+        """
         for customer in self.customers:
             for attribute_name, attribute_value in customer.__dict__.items():
                 if attribute_name == argument_name and attribute_value == argument_value:
@@ -88,12 +108,33 @@ class InMemoryStorage(StorageStrategy):
 
     def update_customer(self, customer: Customer, full_name: str, position: str, name_of_the_organization: str,
                         email: str, phone: str) -> None:
+        """
+        Updates the customer instance in the storage
+        :param customer: Customer
+        :param full_name: surname, name, patronymic of the customer
+        :param position: customer position
+        :param name_of_the_organization: name of company
+        :param email: Customer email address
+        :param phone: customer's phone number
+        :return: None
+        """
         customer.update(full_name, position, name_of_the_organization, email, phone)
 
-    def delete_customer(self, customer):
+    def delete_customer(self, customer: Customer) -> None:
+        """
+        Remove the customer instance in the storage
+        :param customer: Customer
+        :return: None
+        """
         self.customers.remove(customer)
 
     def list_of_customer(self, sort_params: list) -> list:
+        """
+        Searches for all customers in the storage
+        and returns the result
+        :param sort_params: list of parameters for sorting
+        :return: List
+        """
         if len(sort_params) == 0:
             return self.customers
         else:
@@ -110,6 +151,12 @@ class XMLStorage(StorageStrategy):
         self.file_name = file_name
 
     def insert_customer(self, customer: Customer) -> None:
+        """
+        Inserts a customer instance into the storage
+        and writes the file
+        :param customer: Customer
+        :return: None
+        """
         tree = ElementTree.parse(self.file_name)
         root = tree.getroot()
         element_customer = ElementTree.Element("customer")
@@ -136,8 +183,14 @@ class XMLStorage(StorageStrategy):
         tree.write(self.file_name)
 
     def find_customer(self, argument_name: str, argument_value: str) -> Customer:
+        """
+        Searches for a customer in the storage by argument name and value
+        and returns the result
+        :param argument_name: the name of the argument to search for
+        :param argument_value: the value of the argument to search for
+        :return: Customer
+        """
         found = False
-
         tree = ElementTree.parse(self.file_name)
         root = tree.getroot()
         for element_customer in root:
@@ -162,6 +215,17 @@ class XMLStorage(StorageStrategy):
 
     def update_customer(self, customer: Customer, full_name: str, position: str, name_of_the_organization: str,
                         email: str, phone: str) -> None:
+        """
+        Updates the customer instance in the storage
+        and writes the file
+        :param customer: Customer
+        :param full_name: surname, name, patronymic of the customer
+        :param position: customer position
+        :param name_of_the_organization: name of company
+        :param email: Customer email address
+        :param phone: customer's phone number
+        :return: None
+        """
         tree = ElementTree.parse(self.file_name)
         root = tree.getroot()
         for element_customer in root:
@@ -183,6 +247,12 @@ class XMLStorage(StorageStrategy):
         tree.write(self.file_name)
 
     def delete_customer(self, customer: Customer) -> None:
+        """
+        Remove the customer instance in the storage
+        and writes the file
+        :param customer: Customer
+        :return: None
+        """
         found = False
         tree = ElementTree.parse(self.file_name)
         root = tree.getroot()
@@ -196,9 +266,14 @@ class XMLStorage(StorageStrategy):
                 return
 
     def list_of_customer(self, sort_params: list) -> list:
+        """
+        Searches for all customers in the storage
+        and returns the result
+        :param sort_params: list of parameters for sorting
+        :return: List
+        """
         customers_all = []
         customer_id, full_name, position, name_of_the_organization, email, phone = '', '', '', '', '', ''
-
         tree = ElementTree.parse(self.file_name)
         root = tree.getroot()
         for element_customer in root:
@@ -225,71 +300,138 @@ class XMLStorage(StorageStrategy):
 
 class DataBaseStorage(StorageStrategy):
     def __init__(self, db_name: str, db_user: str, db_password: str, db_host: str, db_port: str) -> None:
-        self.connection = create_connection(db_name, db_user, db_password, db_host, db_port)
+        self.db_name = db_name
+        self.db_user = db_user
+        self.db_password = db_password
+        self.db_host = db_host
+        self.db_port = db_port
 
     def insert_customer(self, customer: Customer) -> None:
-        query = (
-            f"""INSERT INTO customers (customer_id, full_name, position, name_of_the_organization, email, phone) 
-            VALUES (
-                    '{customer.customer_id}', 
-                    '{customer.full_name}',
-                    '{customer.position}',
-                    '{customer.name_of_the_organization}',
-                    '{customer.email}',
-                    '{customer.phone}'
-                    );"""
-                )
-        self.connection.autocommit = True
-        cursor = self.connection.cursor()
-        cursor.execute(query)
+        """
+        Inserts a customer instance into the storage
+        :param customer: Customer
+        :return: None
+        """
+        query = f"""
+        INSERT INTO 
+            customers (customer_id, full_name, position, name_of_the_organization, email, phone) 
+        VALUES (
+            '{customer.customer_id}', 
+            '{customer.full_name}',
+            '{customer.position}',
+            '{customer.name_of_the_organization}',
+            '{customer.email}',
+            '{customer.phone}'
+            );
+        """
+        with create_connection(self.db_name, self.db_user, self.db_password, self.db_host, self.db_port) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
 
     def find_customer(self, argument_name: str, argument_value: str) -> str:
-        query = f"SELECT * from customers WHERE customers.{argument_name} = '{argument_value}';"
-        cursor = self.connection.cursor()
-        cursor.execute(query)
-        result = cursor.fetchone()
-        return result
+        """
+        Searches for a customer in the storage by argument name and value
+        and returns the result
+        :param argument_name: the name of the argument to search for
+        :param argument_value: the value of the argument to search for
+        :return: Customer
+        """
+        query = f"""
+        SELECT *
+        FROM customers 
+        WHERE 
+            customers.{argument_name} = '{argument_value}';
+        """
+        with create_connection(self.db_name, self.db_user, self.db_password, self.db_host, self.db_port) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchone()
+                return result
 
     def update_customer(self, customer: Customer, full_name: str, position: str, name_of_the_organization: str,
                         email: str, phone: str) -> None:
-        query = f"""UPDATE customers 
-                SET 
-                    full_name = '{full_name}',
-                    position = '{position}',
-                    name_of_the_organization = '{name_of_the_organization}',
-                    email = '{email}',
-                    phone = '{phone}'
-                WHERE customer_id = '{customer.customer_id}';"""
-        cursor = self.connection.cursor()
-        cursor.execute(query)
+        """
+        Updates the customer instance in the storage
+        :param customer: Customer
+        :param full_name: surname, name, patronymic of the customer
+        :param position: customer position
+        :param name_of_the_organization: name of company
+        :param email: Customer email address
+        :param phone: customer's phone number
+        :return: None
+        """
+        query = f"""
+        UPDATE customers 
+        SET 
+            full_name = '{full_name}',
+            position = '{position}',
+            name_of_the_organization = '{name_of_the_organization}',
+            email = '{email}',
+            phone = '{phone}'
+        WHERE 
+            customer_id = '{customer.customer_id}';
+        """
+        with create_connection(self.db_name, self.db_user, self.db_password, self.db_host, self.db_port) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
 
     def delete_customer(self, customer: Customer) -> None:
-        query = f"DELETE FROM customers WHERE customer_id = '{customer.customer_id}'"
-        cursor = self.connection.cursor()
-        cursor.execute(query)
+        """
+        Remove the customer instance in the storage
+        and writes the file
+        :param customer: Customer
+        :return: None
+        """
+        query = f"""
+        DELETE 
+        FROM customers 
+        WHERE 
+            customer_id = '{customer.customer_id}'
+        """
+        with create_connection(self.db_name, self.db_user, self.db_password, self.db_host, self.db_port) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
 
     def list_of_customer(self, sort_params: list) -> list:
+        """
+        Searches for all customers in the storage
+        and returns the result
+        :param sort_params: list of parameters for sorting
+        :return: List
+        """
         if len(sort_params) == 0:
-            query = "SELECT * from customers;"
+            query = """
+            SELECT * 
+            FROM customers;
+            """
         else:
             param = ",".join(sort_params)
-            query = f"SELECT * from customers ORDER BY {param};"
-        cursor = self.connection.cursor()
-        cursor.execute(query)
-        result = cursor.fetchall()
-        return result
+            query = f"""
+            SELECT * 
+            FROM customers 
+            ORDER BY 
+                {param};
+            """
+        with create_connection(self.db_name, self.db_user, self.db_password, self.db_host, self.db_port) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                return result
 
 
-class CustomerService:
-    def __init__(self, storage: StorageStrategy) -> None:
-        self._storage = storage
-
+class StorageFactory:
     @staticmethod
-    def storage(sys_arguments):
+    def get_storage(sys_arguments) -> StorageStrategy:
+        """
+        Selects storage based on input arguments
+        """
         if sys_arguments.path is not None:
             return XMLStorage(sys_arguments.path)
         elif sys_arguments.db is not None:
-            return create_connection(
+            return DataBaseStorage(
                 sys_arguments.db,
                 sys_arguments.user,
                 sys_arguments.password,
@@ -297,26 +439,76 @@ class CustomerService:
                 sys_arguments.port
             )
         else:
-            return InMemoryStorage
+            return InMemoryStorage()
+
+
+class CustomerService:
+    def __init__(self, storage: StorageStrategy) -> None:
+        self._storage = storage
 
     def create_customer(self, customer_id: str, full_name: str, position: str, name_of_the_organization: str,
                         email: str, phone: str) -> None:
+        """
+        Checks for the existence of a customer
+        Raises 'CustomerException' exception if the customer exists
+        Calls the 'insert_customer' command to insert the customer into the storage
+        :param customer_id: customer ID
+        :param full_name: surname, name, patronymic of the customer
+        :param position: customer position
+        :param name_of_the_organization: name of company
+        :param email: Customer email address
+        :param phone: customer's phone number
+        :return: None
+        """
+        expected_customer = self._storage.find_customer("customer_id", customer_id)
+        if expected_customer is not None:
+            raise CustomerException("Customer already exists")
         customer = Customer(customer_id, full_name, position, name_of_the_organization, email, phone)
         self._storage.insert_customer(customer)
 
-    def display_customer_details(self, argument_name: str, argument_value: str) -> None:
+    def find_customer(self, argument_name: str, argument_value: str) -> Customer:
+        """
+        Calls the 'find_customer' command to find the customer into the storage
+        and return result
+        :param argument_name: the name of the argument to search for
+        :param argument_value: the value of the argument to search for
+        :return:
+        """
         customer = self._storage.find_customer(argument_name, argument_value)
-        print(customer)
+        return customer
 
     def update_customer(self, customer_id: str, full_name: str, position: str, name_of_the_organization: str,
                         email: str, phone: str) -> None:
+        """
+        Searches for a customer by ID  and
+        calls the 'update_customer' command to update the customer into the storage
+        :param customer_id: customer ID
+        :param full_name: surname, name, patronymic of the customer
+        :param position: customer position
+        :param name_of_the_organization: name of company
+        :param email: Customer email address
+        :param phone: customer's phone number
+        :return: None
+        """
         customer = self._storage.find_customer('customer_id', customer_id)
         self._storage.update_customer(customer, full_name, position, name_of_the_organization, email, phone)
 
     def remove_customer(self, customer_id: str) -> None:
+        """
+        Searches for a customer by ID  and
+        calls the 'delete_customer' command to remove the customer into the storage
+        :param customer_id: customer ID
+        :return: None
+        """
         customer = self._storage.find_customer('customer_id', customer_id)
         self._storage.delete_customer(customer)
 
-    def display_customer_data(self, sort_params: list) -> None:
+    def get_list_of_customers(self, sort_params: list) -> list:
+        """
+        Calls the 'list_of_customer' command to find the customer into the storage
+        and return result
+        :param sort_params: list of parameters for sorting
+        :return: List
+        """
         customer_data = self._storage.list_of_customer(sort_params)
-        print(*customer_data, sep='\n')
+        return customer_data
