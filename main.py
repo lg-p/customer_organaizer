d2d2a8778ffaa4ad6f8a4195a9ff87cb0f985a47
alena <alena@lagoshny.ru>
2021-03-29 10:47:03 +0300

    Revert "added comments to methods and display of the result"
    This reverts commit e3f404af

--- main.py
+++ main.py
@@ -2,18 +2,7 @@ import argparse
 
 from handbook.command_parser import ExitCommand, HelpCommand, InsertCommand, FindCommand, UpdateCommand, \
     DeleteCommand, ListCommand
-from handbook.customer_service import CustomerException, CustomerService, StorageFactory
-from handbook.validator import ValidateException
-
-EXPECTED_COMMANDS = {
-    'exit': ExitCommand,
-    'help': HelpCommand,
-    'insert': InsertCommand,
-    'find': FindCommand,
-    'update': UpdateCommand,
-    'delete': DeleteCommand,
-    'list': ListCommand,
-}
+from handbook.customer_service import CustomerService
 
 
 def main():
@@ -26,23 +15,23 @@ def main():
     arg_parser.add_argument('--port', type=str, help='port')
     args = arg_parser.parse_args()
 
-    storage = StorageFactory.get_storage(args)
+    storage = CustomerService.storage(args)
     customer_service = CustomerService(storage)
+
+    expected_commands = {
+        'exit': ExitCommand(),
+        'help': HelpCommand(),
+        'insert': InsertCommand(),
+        'find': FindCommand(),
+        'update': UpdateCommand(),
+        'delete': DeleteCommand(),
+        'list': ListCommand(),
+    }
 
     while True:
         input_command = input("Please enter the command:").split(maxsplit=1)
-        try:
-            command = EXPECTED_COMMANDS[input_command[0]]()
-            command.execute(customer_service)
-        except KeyError:
-            print("INVALID COMMAND!\n")
-            EXPECTED_COMMANDS["help"]().execute(customer_service)
-        except ValidateException as e:
-            print("ERROR:", e)
-        except CustomerException as e:
-            print("ERROR:", e)
-        except Exception as e:
-            print(e)
+        command = expected_commands[input_command[0]]
+        command.execute(customer_service)
 
 
 if __name__ == '__main__':
