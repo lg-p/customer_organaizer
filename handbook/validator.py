e94bd322202cc8f05d2bfe0403cbe0696b69e7a9
alena <alena@lagoshny.ru>
2021-03-29 10:32:53 +0300

    added a new class to return the validation result changed the return result in the Validator class added method description

--- handbook/validator.py
+++ handbook/validator.py
@@ -2,42 +2,64 @@ import re
 
 
 class ValidateException(Exception):
-    pass
+    def __init__(self, message: str) -> None:
+        self.message = message
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class ValidationResult:
+    def __init__(self):
+        self.isSuccess = True
+        self.errors = []
 
 
 class Validator:
     @classmethod
-    def validate_data(cls, name: str, value: str) -> bool:
+    def validate_data(cls, name: str, value: str) -> ValidationResult:
+        """
+        Validates the argument value
+        and stores the validation result and error message in an instance of the 'ValidationResult'
+        :return ValidationResult
+        """
+        result = ValidationResult()
         if name == "customer_id":
             if not cls.validate_number(value):
-                print(f"{name} must be 9 digits long")
-                return False
+                result.isSuccess = False
+                result.errors.append(f"{name} must be 9 digits long")
         elif name == "full_name" or name == "position":
             if not cls.validate_string(value):
-                print(f"{name} must be up to 120 letters long")
-                return False
+                result.isSuccess = False
+                result.errors.append(f"{name} must be up to 120 letters long")
         elif name == "name_of_the_organization":
             if not cls.validate_limited_length_string(value):
-                print(f"{name} must be up to 120 characters long")
-                return False
+                result.isSuccess = False
+                result.errors.append(f"{name} must be up to 120 characters long")
         elif name == "email":
             if not cls.validate_email(value):
-                print("Invalid email template")
-                return False
+                result.isSuccess = False
+                result.errors.append("Invalid email template")
         elif name == "phone":
             if not cls.validate_phone(value):
-                print(f"{name} must be no more than 11 digits")
-                return False
-        return True
+                result.isSuccess = False
+                result.errors.append(f"{name} must be no more than 11 digits")
+        return result
 
     @staticmethod
-    def validate_data_for_list(arguments: list) -> bool:
+    def validate_data_for_list(arguments: list) -> ValidationResult:
+        """
+        Validates a list of arguments
+        and stores the validation result and error message in an instance of the 'ValidationResult'
+        :return ValidationResult
+        """
+        result = ValidationResult()
         available_arguments = ["customer_id", "full_name", "position", "name_of_the_organization", "email", "phone"]
         for argument in arguments:
             if argument not in available_arguments:
-                print(f"'list' command has no argument: {argument}")
-                return False
-        return True
+                result.isSuccess = False
+                result.errors.append(f"'list' command has no argument: {argument}")
+        return result
 
     @staticmethod
     def validate_string(value: str) -> bool:
